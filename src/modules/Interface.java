3876e9ce80ac382526fc4127fb62318da4199d71
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2024-07-15 01:08:51 -0300

    Update Interface.java

--- src/modules/Interface.java
+++ src/modules/Interface.java
@@ -6,6 +6,6 @@ public interface Interface {
     ArrayList<Cliente> clientes = new ArrayList<>();
     ArrayList<Motorista> motoristas = new ArrayList<>();
     ArrayList<Carro> carros = new ArrayList<>();
-    ArrayList<Agencia> produtos = new ArrayList<>();
+    ArrayList<Agencia> agencias = new ArrayList<>();
     ArrayList<Aluga> locacoes = new ArrayList<>();
 }
